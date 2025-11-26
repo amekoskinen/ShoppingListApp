6f0a735fceb6d8a2c5ef7d43a7855e8d98bd1d5e
amekoskinen <amekoskinen@hotmail.com>
2025-11-26 13:52:07 +0200

    Added class check to data scraping

--- data_scraping.py
+++ data_scraping.py
@@ -1,16 +1,18 @@
 import time
 from selenium.webdriver.common.by import By
 import pandas
+from datacheck import check_class
 
 class DataScraping:
     def __init__(self, driver):
         self.driver = driver
         self.product_price = {}
+        self.class_checked = check_class()
 
     def get_information(self, url, all_products):
         self.driver.get(url)
         time.sleep(5)
-        products = self.driver.find_elements(By.CSS_SELECTOR, ".joHiJE")
+        products = self.driver.find_elements(By.CSS_SELECTOR, self.class_checked)
         product_list = []
         for product in products:
             product_list.append(product.text)
--- datacheck.py
+++ datacheck.py
@@ -1,13 +1,13 @@
 import requests
 
-response = requests.get("https://www.s-kaupat.fi/tuotteet/hedelmat-ja-vihannekset-1/vihannekset/kurkut")
-test_text = response.text
+def check_class():
+    response = requests.get("https://www.s-kaupat.fi/tuotteet/hedelmat-ja-vihannekset-1/vihannekset/kurkut")
+    test_text = response.text
 
-test = test_text.split(" ")
-index = test.index('data-test-id="product-card"')
+    test = test_text.split(" ")
+    index = test.index('data-test-id="product-card"')
 
-class_checked = "."+test[index-1][:-1]
-print(index)
-print(test[index-1])
+    class_checked = "."+test[index-1][:-1]
 
-print(class_checked)
+    return class_checked
+
